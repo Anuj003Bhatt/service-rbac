ea4c41caeb46ea3f546632f49840207d4b665df4
Anuj003Bhatt <anujnarayanbhatt@MacBook-Pro.local>
2025-03-22 19:14:13 +0530

    Redoc API Documentation

---
+++ src/main/java/com/decimal/rbac/controller/ApiDocumentation.java
@@ -0,0 +1,38 @@
+package com.decimal.rbac.controller;
+
+import com.decimal.rbac.exceptions.BadRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+@RestController
+@RequiredArgsConstructor
+public class ApiDocumentation {
+    private static final String DOC_RESOURCE_FILE_PATH="classpath:ApiDoc.html";
+    private final ResourceLoader resourceLoader;
+    @Value("${springdoc.api-docs.path}")
+    private String apiDocYamlPath;
+
+    @GetMapping("documentation")
+    public String getApiDocumentation(
+            @RequestHeader String host
+    ) {
+        String url = ServletUriComponentsBuilder.fromCurrentContextPath().path(apiDocYamlPath).build().toUriString();
+        Resource resource = resourceLoader.getResource(DOC_RESOURCE_FILE_PATH);
+        try {
+           return Files.readString(resource.getFile().toPath()).replace(
+                   "%doc_uri%", url
+           );
+        } catch (IOException e) {
+            throw new BadRequestException("Unable to load API Documentation. Please try again later.");
+        }
+    }
+}
--- src/main/java/com/decimal/rbac/controller/advice/RestExceptionHandler.java
+++ src/main/java/com/decimal/rbac/controller/advice/RestExceptionHandler.java
@@ -3,7 +3,6 @@ package com.decimal.rbac.controller.advice;
 import com.decimal.rbac.exceptions.AuthenticationFailedException;
 import com.decimal.rbac.exceptions.BadRequestException;
 import com.decimal.rbac.exceptions.NotFoundException;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,7 +15,7 @@ import java.util.Map;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {NotFoundException.class, ResourceNotFoundException.class})
+    @ExceptionHandler(value = {NotFoundException.class})
     protected ResponseEntity<Object> handle404(RuntimeException ex, WebRequest request) {
         return responseFromError(ex, HttpStatus.NOT_FOUND);
     }
